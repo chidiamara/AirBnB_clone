9a6fe52190b3d8202385f03c6cf04f3dd282d14f
Chidi <chidiamaraekejiuba@gmail.com>
2023-03-13 14:12:55 +0000

    Other class objects

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains Amenity class """
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Class Amenity """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains State class """
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ Class State """
+    name = ""
