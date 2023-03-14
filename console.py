942ce2b048cea5384c5f36799b1e500d3aff78dd
“Okunade <bidemiokunade@gmail.com>
2023-03-14 02:02:49 -0700

    Console file for task one

---
+++ console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Here We Defines the HBnB console."""
+import cmd
+import re
+from shlex import split
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HBNB command interpreter.
+        Attributes:
+            prompt (str): The command prompt
+    """
+    pass
